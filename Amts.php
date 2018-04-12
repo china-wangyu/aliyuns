5dd3a987ad8f00a10a6efb03f0c0b93343e1fb23
china_wangyu <china_wangyu@aliyun.com>
2018-04-12 16:30:26 +0800

    由wene发布, 版本号：1.0.2, 阿里云-PHP媒体处理转码 2018-4-12 16:30

--- Amts.php
+++ Amts.php
@@ -17,110 +17,175 @@ use Mts\Request\V20140618 as Mts;
  * Class AliyunMts
  * @package Aliyun 阿里云SDK库
  * @author china_wangyu@aliyun.com
+ * @inheritdoc 使用说明
+ * 先设置参数以下
+ *  \aliyuns\Amts::$mps_region_id = ''
+ *  \aliyuns\Amts::$access_key_id = ''
+ *  \aliyuns\Amts::$access_key_secret = ''
+ *  \aliyuns\Amts::$template_id = ''
+ *  \aliyuns\Amts::$oss_location = ''
+ *  \aliyuns\Amts::$oss_bucket = ''
+ *  \aliyuns\Amts::$oss_input_object = ''
+ *  \aliyuns\Amts::$oss_output_object = ''
+ *  \aliyuns\Amts::init();  // 执行提交作业进行  - 转码任务
  */
 class Amts
 {
+    /**
+     * 所属地区 默认：深圳
+     * @var string
+     */
+    static public $mps_region_id = 'cn-shenzhen';
+    /**
+     * 阿里云access_key_id
+     * @var string
+     */
+    static public $access_key_id = '';
+    /**
+     * 阿里云access_key_secret
+     * @var string
+     */
+    static public $access_key_secret = '';
+    /**
+     * 模板ID 可选填
+     * @defualt : S00000001-200010
+     * @var string
+     */
+    static public $template_id = 'S00000001-200010';
+    /**
+     *  所属地区管道pipeline_id
+     * @var string
+     */
+    static public $pipeline_id = '';
+    /**
+     * 所属地区oss站点
+     * @var string
+     */
+    static public $oss_location = 'oss-cn-shenzhen';
+    /**
+     * 阿里云OSS bucket名称
+     * @var string
+     */
+    static public $oss_bucket = '';
+    /**
+     * 阿里云OSS 需要转码视频文件地址
+     * @var string
+     * @type oos-filePath
+     */
+    static public $oss_input_object = '';
+    /**
+     * 阿里云OSS 需要 ·输出· 转码视频文件地址
+     * @var string
+     * @type oos-filePath
+     */
+    static public $oss_output_object='';
+    /**
+     * 转码输出对象初始化
+     * @var array
+     */
+    static private $outputs = [];
+    /**
+     * DefaultAcsClient实例并初始化对象
+     * @object \DefaultAcsClient
+     */
+    static private $client = [];
+    /**
+     * 创建API请求对象
+     * @object  \Mts\SubmitJobsRequest
+     */
+    static private $request = [];
 
-
-    public static function init(string $inputObj = 'video/1.mp4',string $outputObj = 'output2.mp4')
+    public static function init()
     {
-        $params = self::initParams($inputObj,$outputObj);
-        $client = self::initClient($params['mps_region_id'], $params['access_key_id'], $params['access_key_secret']);
-        $outputs = self::initOutput($params['oss_output_object'], $params['template_id']);
-        $request = self::initRequest($params, $outputs);
-        self::exec($client, $request);
+        self::initParam();
+        self::getPipelineId();
+        self::initClient();
+        self::initOutput();
+        self::initRequest();
+        self::exec(self::$client, self::$request);
     }
 
     /**
+     * 检验参数，进行污水处理
+     */
+    public static function initParam()
+    {
+        self::$mps_region_id == '' && self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'mps_region_id is null']);
+        self::$access_key_id == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'access_key_id is null']);
+        self::$access_key_secret == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'access_key_secret is null']);
+        self::$template_id == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'template_id is null']);
+        self::$oss_location == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'oss_location is null']);
+        self::$oss_bucket == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'oss_bucket is null']);
+        self::$oss_input_object == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'oss_input_object is null']);
+        self::$oss_output_object == ''&&self::msg(400,'BAD REQUEST',['Error: ' => '参数错误' ,' Message' => 'ss_output_object is null']);
+    }
+
+    /**
+     * 获取管道ID
      * 获取pipeline_id
-     * @param $mps_region_id
-     * @param $access_key_id
-     * @param $access_key_secret
      * @return mixed
      */
-    public static function getPipelineId($mps_region_id, $access_key_id, $access_key_secret)
+    public static function getPipelineId()
     {
-        $iClientProfile = \DefaultProfile::getProfile($mps_region_id, $access_key_id, $access_key_secret);
-        $client = new \DefaultAcsClient($iClientProfile);
-        $request = new Mts\SearchPipelineRequest();
-        $response = $client->getAcsResponse($request);
-        $pipelines = json_decode(json_encode($response->PipelineList->Pipeline), true);
-//        echo '<pre>';
-//        echo $pipelines[0]['Id'];
-        return $pipelines[0]['Id'];
+        session('pipeline_id') != ''&& self::$pipeline_id =session('pipeline_id');
+        if (self::$pipeline_id == ''){
+            $iClientProfile = \DefaultProfile::getProfile(self::$mps_region_id, self::$access_key_id, self::$access_key_secret);
+            $client = new \DefaultAcsClient($iClientProfile);
+            $request = new Mts\SearchPipelineRequest();
+            $response = $client->getAcsResponse($request);
+            $pipelines = json_decode(json_encode($response->PipelineList->Pipeline), true);
+            session_start();
+            session('pipeline_id',$pipelines[0]['Id']);
+            self::$pipeline_id = $pipelines[0]['Id'];
+        }
     }
 
-
-    /**
-     * 初始化请求资源参数
-     * @return mixed
-     */
-    public static function initParams(string $inputObj,string $outputObj)
-    {
-        $param['mps_region_id'] = 'cn-shenzhen';
-        $param['access_key_id'] = 'LTAIl04lLlhmw38Q';
-        $param['access_key_secret'] = 'KDvJMzVyR3tQoPogaDcYvhokZXbyi1';
-        $param['pipeline_id'] = self::getPipelineId($param['mps_region_id'], $param['access_key_id'], $param['access_key_secret']);
-        $param['template_id'] = 'S00000001-200010';
-        $param['oss_location'] = 'oss-cn-shenzhen';
-        $param['oss_bucket'] = 'tiaoba';
-        $param['oss_input_object'] = $inputObj;
-        $param['oss_output_object'] = $outputObj;
-        return $param;
-    }
 
     /**
      * 创建DefaultAcsClient实例并初始化
-     * @param $mps_region_id
-     * @param $access_key_id
-     * @param $access_key_secret
-     * @return \DefaultAcsClient
+     * @self::$client \DefaultAcsClient
      */
-    public static function initClient($mps_region_id, $access_key_id, $access_key_secret)
+    public static function initClient()
     {
         # 创建DefaultAcsClient实例并初始化
         $clientProfile = \DefaultProfile::getProfile(
-            $mps_region_id,                   # 您的 Region ID
-            $access_key_id,                   # 您的 AccessKey ID
-            $access_key_secret                # 您的 AccessKey Secret
+            self::$mps_region_id,                   # 您的 Region ID
+            self::$access_key_id,                   # 您的 AccessKey ID
+            self::$access_key_secret                # 您的 AccessKey Secret
         );
-        $client = new \DefaultAcsClient($clientProfile);
-        return $client;
+        self::$client = new \DefaultAcsClient($clientProfile);
     }
 
     /**
      * 创建API请求并设置参数
-     * @param $param
-     * @return Mts\SubmitJobsRequest
+     * @self::$request Mts\SubmitJobsRequest
      */
-    public static function initRequest($param, $outputs)
+    public static function initRequest()
     {
         # 创建API请求并设置参数
         $request = new Mts\SubmitJobsRequest();
         $request->setAcceptFormat('JSON');
         # Input
-        $input = array('Location' => $param['oss_location'],
-            'Bucket' => $param['oss_bucket'],
-            'Object' => urlencode($param['oss_input_object']));
+        $input = array('Location' => self::$oss_location,
+            'Bucket' => self::$oss_bucket,
+            'Object' => urlencode(self::$oss_input_object));
         $request->setInput(json_encode($input));
-        $request->setOUtputs(json_encode($outputs));
-        $request->setOutputBucket($param['oss_bucket']);
-        $request->setOutputLocation($param['oss_location']);
+        $request->setOUtputs(json_encode(self::$outputs));
+        $request->setOutputBucket(self::$oss_bucket);
+        $request->setOutputLocation(self::$oss_location);
         # PipelineId
-        $request->setPipelineId($param['pipeline_id']);
-        return $request;
+        $request->setPipelineId(self::$pipeline_id);
+        self::$request = $request;
     }
 
     /**
      * 初始化输出资源类型
-     * @param $oss_output_object
-     * @param $template_id
      * @return array
      */
-    public static function initOutput($oss_output_object, $template_id)
+    public static function initOutput()
     {
         # Output
-        $output = array('OutputObject' => urlencode($oss_output_object));
+        $output = array('OutputObject' => urlencode(self::$oss_output_object));
         # Ouput->Container
         $output['Container'] = array('Format' => 'mp4');
         # Ouput->Video
@@ -134,7 +199,7 @@ class Amts
             'Channels' => 2,
             'Samplerate' => 44100);
         # Ouput->TemplateId
-        $output['TemplateId'] = $template_id;
+        $output['TemplateId'] = self::$template_id;
         $outputs = array($output);
         return $outputs;
     }
